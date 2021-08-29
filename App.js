c791e940ee6491e9b7dde8677c7e790bae5d08f4
akshay.kumar <akshaymain95@gmail.com>
2021-08-29 18:36:16 +0530

    react native project

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { View, Text, Image, StyleSheet, FlatList, Alert } from 'react-native'
+import { v4 as uuid } from 'uuid';
+import Header from './components/Header';
+import ListView from './components/ListView'
+import AddItem from './components/AddItem';
+
+
+const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Eggs'
+    },
+    {
+      id: uuid(),
+      text: 'Ground nut'
+    },
+    {
+      id: uuid(),
+      text: 'Chocolates'
+    },
+    {
+      id: uuid(),
+      text: 'Ice cream'
+    },
+  ])
+
+  const deleteItem = (id) => {
+    setItems(prevItems => {
+      return prevItems.filter((item) => item.id != id)
+    })
+  }
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item' ,[{text: 'Ok'}])
+    } else {
+      setItems(prevItems => {
+        return [{ id: uuid(), text }, ...prevItems]
+      })
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={
+          ({ item }) => <ListView item={item} deleteItem={deleteItem}></ListView>
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+})
+
+export default App;
